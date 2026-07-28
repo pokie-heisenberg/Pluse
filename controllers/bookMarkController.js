828f4153896025d7d60419a071e14c8ac6bc9011
pokie-heisenberg <amanyadavheisenberg@gmail.com>
2026-07-28 11:35:48 +0530

    feat: enhance getBookMarkPosts - populate user details in bookmarks response

--- controllers/bookMarkController.js
+++ controllers/bookMarkController.js
@@ -2,7 +2,9 @@ const catchAsyncError = require('./../utils/catchAsyncError');
 const appError = require('./../utils/appError');
 const BookMark = require('./../models/bookMarkModels');
 exports.getBookMarkPosts = catchAsyncError(async (req, res, next) => {
-  const bookMarks = await BookMark.find({ user: req.user.id }).populate('post');
+  const bookMarks = await BookMark.find({ user: req.user.id })
+    .populate('post')
+    .populate('user');
   res.status(200).json({
     status: 'success',
     result: bookMarks.length,
